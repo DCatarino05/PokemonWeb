823c25c9cc15b6e940cbab2f0f445f25b68c946d
Rafael T. <93728929+Rafaa05@users.noreply.github.com>
2024-04-30 15:08:52 +0100

    .

--- js/login.js
+++ js/login.js
@@ -32,3 +32,43 @@ document.getElementById("teamForm").addEventListener("submit", function(event) {
       });
     });
   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // Selecione a imagem da Pokédex
+    const pokedexImage = document.querySelector('.pokedex');
+  
+    // Adicione um evento de mudança para cada botão de rádio
+    document.querySelectorAll('input[name="team"]').forEach((radio) => {
+      radio.addEventListener('change', () => {
+        if (radio.checked) {
+          // Atualiza a imagem da Pokédex de acordo com a equipe selecionada
+          switch (radio.value) {
+            case 'red':
+              pokedexImage.src = "./images/timevalor.png";
+              break;
+            case 'blue':
+              pokedexImage.src = "./images/timeazul.png";
+              break;
+            case 'yellow':
+              pokedexImage.src = "./images/timeamarela.png";
+              break;
+            default:
+              pokedexImage.src = "./images/pokedex.png";
+          }
+  
+          // Exibe a imagem da Pokédex quando uma equipe for selecionada
+          pokedexImage.style.display = "block";
+        }
+      });
+    });
+  
+    // Adicione um evento de envio para o formulário
+    document.getElementById("teamForm").addEventListener("submit", function(event) {
+      event.preventDefault();
+      const selectedTeam = document.querySelector('input[name="team"]:checked').value;
+      localStorage.setItem('pokemonTeam', selectedTeam);
+      window.location.href = "pokedex.html"; 
+    });
+  });
+  
+
